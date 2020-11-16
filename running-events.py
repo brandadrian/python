24d68f9562d0473677204dca637d5dafd0bfae2c
brandadr <brandadrian@gmail.com>
2020-11-16 20:12:22 +0100

    Create running-events.py

---
+++ running-events.py
@@ -0,0 +1,29 @@
+import os
+import json
+from pymongo import MongoClient
+from flask import Flask, request
+
+app = Flask(__name__)
+usr = 'root' #os.environ['MONGO_DB_USER']
+pwd = 'root' #os.environ['MONGO_DB_PASS']
+client = MongoClient('mongodb+srv://' + usr + ':' + pwd + '@cluster0.srbj2.mongodb.net/running-event-db?retryWrites=true&w=majority')
+db = client['running-event-db']
+collection = db['running-event']
+
+@app.route('/running-events')
+def get():
+    documents = collection.find()
+    response = []
+    for document in documents:
+        document['_id'] = str(document['_id'])
+        response.append(document)
+    return json.dumps(response)
+
+@app.route('/running-events', methods=['POST'])
+def post():
+    req_data = request.get_json()
+    collection.insert_one(req_data).inserted_id
+    return ('', 200)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
